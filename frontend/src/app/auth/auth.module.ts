9dcab76468e718236a619f2a0a5dc0f083e85d5f
Carlos Hidalgo Risco <carloshidalgorisco@gmail.com>
2022-04-23 00:37:13 +0200

    feat: add login reCaptcha

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 
 @NgModule({
@@ -11,7 +11,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxCaptchaModule
   ]
 })
 export class AuthModule { }
--- frontend/src/app/auth/pages/login/login.component.ts
+++ frontend/src/app/auth/pages/login/login.component.ts
@@ -14,34 +14,40 @@ export class LoginComponent implements OnInit {
 
   formLogin: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
+    password: ['', [Validators.required]],
+    recaptcha: ['', Validators.required]
   });
+  siteKey: string = "6LeC2ZMfAAAAAEE8Z1L4cLhr8IZVMLmdu_WRu5Zp"
 
-  errorMsg:string = ""
-  errorEmail:string = ""
-  errorPassword:string =""
+  errorMsg: string = ""
+  errorEmail: string = ""
+  errorPassword: string = ""
 
-  constructor(private fb: FormBuilder, 
-              private authService: AuthService,
-              private router:Router) { }
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    
+
     const { email, password } = this.formLogin.value;
 
-    this.authService.login(email,password)
+    this.authService.login(email, password)
       .subscribe(
         {
           next: (ok) => {
-            if (ok === true) {
+            if (ok === true && this.formLogin.valid) {
               //comprobar el tipo de usuario
               this.router.navigate(['/dashboard']);
-            }else{
-              this.errorMsg = "Usuario o contraseña incorrectos"
-              Swal.fire('Error', ok.msg, 'error')
+            } else {
+              if (this.formLogin.controls['recaptcha'].errors) {
+                Swal.fire('Error','Es necesario completar el captcha', 'error')
+              } else {
+                this.errorMsg = "Usuario o contraseña incorrectos"
+                Swal.fire('Error', ok.msg, 'error')
+              }
             }
           }
         }
